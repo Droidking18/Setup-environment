e5ea726f42398cbb82518aa398bfab5c25a181fd
Dovi KAPLAN <dkaplan@e5r11p5.wethinkcode.co.za>
2018-05-29 15:50:45 +0200

    Left out some stuff

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 echo "Starting setup"
 rm ~/.zshrc
 mv .zshrc ~/.zshrc
+source ~/.zshrc
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
+echo "set number" >> ~/.vimrc
 echo "Complete"
