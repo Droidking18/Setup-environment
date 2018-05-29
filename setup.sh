84e12bd3202f305c1634855e7255e68a7782e124
Dovi KAPLAN <dkaplan@e5r11p5.wethinkcode.co.za>
2018-05-29 16:15:29 +0200

    I'm sick of commenting

--- setup.sh
+++ setup.sh
@@ -3,5 +3,7 @@ cp .zshrc ~/.zshrc
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 echo "set number" >> ~/.vimrc
-source ~/.zshrc
+bash s.sh
+git clone https://github.com/jgigault/42FileChecker ~/42FileChecker
+cd ~/42FileChecker && bash ./42FileChecker.sh
 echo "Complete"
