98ec204a5fa51509621561d4fba13bea2f97b7a2
Dovi KAPLAN <dkaplan@e5r11p5.wethinkcode.co.za>
2018-05-29 16:23:04 +0200

    Add

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@ cp .zshrc ~/.zshrc
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 echo "set number" >> ~/.vimrc
-bash s.sh
+. ~/zshrc
 git clone https://github.com/jgigault/42FileChecker ~/42FileChecker
-cd ~/42FileChecker && bash ./42FileChecker.sh
-echo "Complete"
+echo "Setup complete"
