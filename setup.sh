625e0b444bd204d86ea71f1c6cbcb733304e71ca
Dovi KAPLAN <dkaplan@e5r11p5.wethinkcode.co.za>
2018-05-29 16:06:32 +0200

    Last one maybe

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 echo "Starting setup"
-rm ~/.zshrc
 cp .zshrc ~/.zshrc
-source ~/.zshrc
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 echo "set number" >> ~/.vimrc
+source ~/.zshrc
 echo "Complete"
